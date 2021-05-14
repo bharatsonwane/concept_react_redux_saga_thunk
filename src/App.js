9928f150fcac6e318f920e094cd5450964dc0ff0
bharatsonwane <bharatrsonwane@gmail.com>
2021-05-14 22:00:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+// import AppReduxMain from './conceptReduxProjects/AppReduxMain';
+import AppSagaMain from './conceptReduxSagaProjects/AppSagaMain';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AppReduxMain /> */}
+      <AppSagaMain />
+    </div>
+  );
+}
+
+export default App;
